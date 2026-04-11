e0c2f5d986afc9a4a8b1ab88ccbd940998cb350a
bananaTiko <111482265+bananaTiko@users.noreply.github.com>
2026-04-10 18:22:31 -0700

    Add System.IO namespace to BlenderPluginViewModel
    Added missing using directive for System.IO.

--- FortnitePorting/ViewModels/Plugin/BlenderPluginViewModel.cs
+++ FortnitePorting/ViewModels/Plugin/BlenderPluginViewModel.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
